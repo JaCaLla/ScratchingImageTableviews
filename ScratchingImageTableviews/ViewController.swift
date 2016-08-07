f0bba4e39ea66ed12e7659850c03dcb6654ff5a4
Javier Calatrava Llavería <javi.calatrava@gmail.com>
2016-08-07 17:09:03 +0200

    Initial XCode project.

---
+++ ScratchingImageTableviews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScratchingImageTableviews
+//
+//  Created by JAVIER CALATRAVA LLAVERIA on 07/08/16.
+//  Copyright © 2016 JAVIER CALATRAVA LLAVERIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
